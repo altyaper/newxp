b9402b87d371fd300342279932b9df6ce25fa30e
altyaper <jorgechavzns@gmail.com>
2026-05-26 18:36:25 -0700

    feat(pre-processing): add clean_html_content with docs

--- lib/newxp/pre_processing.ex
+++ lib/newxp/pre_processing.ex
@@ -1,2 +1,21 @@
 defmodule Newxp.PreProcessing do
+  @doc "Apply core HTML cleaning transformations."
+  def clean_html_content(html) do
+    {:ok, doc} = Floki.parse_document(html)
+
+    doc
+    |> remove_figures()
+    |> remove_tables()
+    |> remove_noscript()
+    |> Floki.raw_html()
+  end
+
+  # Removes all <figure> elements from the parsed document.
+  defp remove_figures(doc), do: Floki.filter_out(doc, "figure")
+
+  # Removes all <table> elements from the parsed document.
+  defp remove_tables(doc), do: Floki.filter_out(doc, "table")
+
+  # Removes all <noscript> elements from the parsed document.
+  defp remove_noscript(doc), do: Floki.filter_out(doc, "noscript")
 end
